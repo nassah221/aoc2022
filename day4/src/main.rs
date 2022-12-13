fdc2f27ddb0b4ad3320bc504ed22a6b6e0ce80d8
Muhammad Hassan <hassanmuhammad221@gmail.com>
2022-12-13 15:18:09 +0500

    add day4 part2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let input = include_str!("../input.txt");
-    let count = input
+    let part_1 = input
         .lines()
         .filter_map(|line| {
             let section = line.split_once(",").unwrap();
@@ -22,5 +22,28 @@ fn main() {
             return None;
         })
         .count();
-    println!("{}", count)
+    let part_2 = input
+        .lines()
+        .filter_map(|line| {
+            let section = line.split_once(",").unwrap();
+            let mut pair_1 = section.0.splitn(2, "-");
+            let mut pair_2 = section.1.splitn(2, "-");
+
+            let (p1x, p1y) = (
+                pair_1.next().unwrap().parse::<usize>().unwrap(),
+                pair_1.next().unwrap().parse::<usize>().unwrap(),
+            );
+            let (p2x, p2y) = (
+                pair_2.next().unwrap().parse::<usize>().unwrap(),
+                pair_2.next().unwrap().parse::<usize>().unwrap(),
+            );
+
+            if (p1x <= p2x && p2x <= p1y) || (p2x <= p1x && p1x <= p2y) {
+                println!("{:?}", section);
+                return Some(1);
+            }
+            return None;
+        })
+        .count();
+    println!("{} {}", part_1, part_2)
 }
